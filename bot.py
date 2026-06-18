e96de3fa8f4f5de5bea119c8f04aca45d29467ce
arina2201 <arinka2201@gmail.com>
2026-06-18 15:38:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,19 @@
-from telegram import Bot
+import os
+import requests
 
-TOKEN = "8947973799:AAEPRDmCpotghvbKhLdJyvU-p0SNMneP5Ts"
+TOKEN = os.environ["8947973799:AAEPRDmCpotghvbKhLdJyvU-p0SNMneP5Ts"]
+CHAT_ID = os.environ["-1003927208269"]
 
-bot = Bot(token=TOKEN)
+def main():
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-updates = bot.get_updates()
+    r = requests.post(url, data={
+        "chat_id": CHAT_ID,
+        "text": "test from GitHub Actions"
+    })
 
-for u in updates:
-    try:
-        print(u.message.chat.id, u.message.chat.title)
-    except:
-        pass
+    print(r.status_code)
+    print(r.text)
+
+if __name__ == "__main__":
+    main()
