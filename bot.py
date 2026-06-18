dbfb7b336a0c59e39d4584e5734aec3962c97fe7
arina2201 <arinka2201@gmail.com>
2026-06-18 15:58:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,44 @@
 import os
 import requests
+from telegram import Bot
+from datetime import datetime, timedelta, timezone
 
 TOKEN = os.environ["TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
+LAT = 40.1872
+LNG = 44.5152
+
+
+def get_sunset():
+    url = f"https://api.sunrise-sunset.org/json?lat={LAT}&lng={LNG}&formatted=0"
+    data = requests.get(url).json()
+
+    sunset_utc = data["results"]["sunset"]
+    sunset = datetime.fromisoformat(sunset_utc.replace("Z", "+00:00"))
+
+    return sunset.astimezone()
+
+
 def main():
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    bot = Bot(token=TOKEN)
 
-    r = requests.post(url, data={
-        "chat_id": CHAT_ID,
-        "text": "test from GitHub Actions"
-    })
+    sunset = get_sunset()
 
-    print(r.status_code)
-    print(r.text)
+    # Ереван UTC+4
+    notify_time = sunset - timedelta(minutes=30)
+
+    now = datetime.now(timezone.utc).astimezone()
+
+    # запускаем только если сейчас >= времени уведомления
+    if now >= notify_time and now < notify_time + timedelta(minutes=10):
+        bot.send_message(
+            chat_id=int(CHAT_ID),
+            text="джаны, закат через 30 минут!"
+        )
+
+    print("done")
+
 
 if __name__ == "__main__":
     main()
